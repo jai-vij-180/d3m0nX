4a71596f8c9a9a84a9e17f51a45810a660a1c6f0
jai-vij-180 <yoyojaivij@gmail.com>
2016-04-17 15:58:52 +0530

    first commit ( movement + animStates)
    added movement, animation states ( to be linked), extra assets imported
    too.

---
+++ Assets/Scripts/Characters/Player/Basics.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basics : MonoBehaviour   {
+
+    public GameObject bullet;
+    public Transform gunPoint;
+    Manager statesManager;
+
+    [Range(0.1f, 0.5f)]
+    public float speed;
+
+
+    Rigidbody2D rb;
+    void Awake()
+    {
+        statesManager = GetComponent<Manager>();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Shooting
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shooting_True();
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        //walking
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            right();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            left();
+        }
+
+    }
+	
+
+    //  -----------------------FUNTIONS---------------------
+
+    
+        void right()   
+        {
+
+            Vector2 p = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + 1f, transform.position.y), speed * Time.deltaTime);
+            rb.MovePosition(p);
+            statesManager.StateChanger(Manager.States.walk, true);
+            
+        }
+
+
+         void left()    //left
+        {
+
+            Vector2 p2 = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - 1f, transform.position.y), speed *Time.deltaTime);
+            rb.MovePosition(p2);
+            statesManager.StateChanger(Manager.States.walk, true);
+            
+        }
+
+
+
+    void Shooting_True()
+    {
+        Instantiate(bullet, gunPoint.position, Quaternion.identity);
+        statesManager.StateChanger(Manager.States.shoot, true);
+    }
+
+    public void Shooting_False()
+    {
+        statesManager.StateChanger(Manager.States.shoot, false);
+    }
+
+    
+}
---
+++ Assets/Scripts/Characters/Player/Manager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+    //This script controls all the animations related to the character JAC1000
+
+    //wanna add a new state? 
+    //declare it inside the enum
+    //pass the state and its bool value from another class based on events.
+    //be sure to add the AnimationEvent in the animation incase it will stop after some frames ( like jump,attack,transformations)
+
+   
+
+    public enum States
+    {
+        idle,
+        walk,
+        shoot,
+        die,
+        hit,
+        run,
+        jump
+    }
+
+
+    public Animator animator;
+
+
+    
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StateChanger(States newState, bool isPlaying)
+    {
+        animator.SetBool(newState.ToString(), isPlaying); //passing the bools 
+    }
+
+}
